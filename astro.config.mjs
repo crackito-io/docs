2ab5b7bd2be4be6f8c6ae5fc8cb92e13c4cf0985
Julien Breton <xm9q8f80@jlnbrtn.me>
2024-04-24 11:44:38 +0200

    add i18n and fix themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,45 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.crackito.io',
-	integrations: [
-		starlight({
-			title: '',
-			logo: {
-				light: './src/assets/logo_docs.webp',
-				dark: './src/assets/logo_docs_light.webp'
-			},
-			social: {
-				github: 'https://github.com/crackito-io/crackito',
-			},
-			sidebar: [
-				{
-					label: 'Overview', link: '/overview'
-				},
-				{
-					label: 'User',
-					autogenerate: { directory: 'user' },
-				},
-				{
-					label: 'Admin',
-					autogenerate: { directory: 'admin' },
-				},
-				{
-					label: 'Developper',
-					autogenerate: { directory: 'developper' },
-				},
-			],
-		}),
-	],
+  site: "https://docs.crackito.io",
+  integrations: [
+    starlight({
+      title: "",
+	  defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+        },
+		fr: {
+			label: "Français",
+		  },
+      },
+      logo: {
+        light: "./src/assets/logo_docs.webp",
+        dark: "./src/assets/logo_docs_light.webp",
+      },
+      social: {
+        github: "https://github.com/crackito-io/crackito",
+      },
+      sidebar: [
+        {
+          label: "Overview",
+          link: "/overview",
+        },
+        {
+          label: "User",
+          autogenerate: { directory: "user" },
+        },
+        {
+          label: "Admin",
+          autogenerate: { directory: "admin" },
+        },
+        {
+          label: "Developper",
+          autogenerate: { directory: "developper" },
+        },
+      ],
+    }),
+  ],
 });
