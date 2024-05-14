317fe7db629aac97471d72480fcba1300325817b
Gamingdy <christgik12@gmail.com>
2024-05-14 10:39:35 +0200

    feat: update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,52 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightLinksValidator from 'starlight-links-validator';
 
 export default defineConfig({
-  site: "https://docs.crackito.io",
-  integrations: [
-    starlight({
-      title: "Crackito",
-      customCss: [
-        './src/styles/custom.css',
-      ],
-	  defaultLocale: "en",
-      locales: {
-        en: {
-          label: "English",
-        },
-		fr: {
-			label: "Français",
-		  },
-      },
-      logo: {
-        light: "./src/assets/logo_docs.webp",
-        dark: "./src/assets/logo_docs_light.webp",
-		replacesTitle: true,
-      },
-      social: {
-        github: "https://github.com/crackito-io/crackito",
-      },
-      sidebar: [
-        {
-          label: "Overview",
-          link: "/overview",
-        },
-        {
-          label: "User",
-          autogenerate: { directory: "user" },
-        },
-        {
-          label: "Admin",
-          autogenerate: { directory: "admin" },
-        },
-        {
-          label: "Developper",
-          autogenerate: { directory: "developper" },
-        },
-      ],
-    }),
-  ],
-  redirects: {
-    '/': '/en'
-  }
+    site: "https://docs.crackito.io",
+    integrations: [
+        starlight({
+            title: "Crackito",
+            customCss: ["./src/styles/custom.css"],
+            plugins: [starlightLinksValidator()],
+            defaultLocale: "en",
+            locales: {
+                en: {
+                    label: "English",
+                },
+                fr: {
+                    label: "Français",
+                },
+            },
+            logo: {
+                light: "./src/assets/logo_docs.webp",
+                dark: "./src/assets/logo_docs_light.webp",
+                replacesTitle: true,
+            },
+            social: {
+                github: "https://github.com/crackito-io/crackito",
+            },
+            sidebar: [
+                {
+                    label: "Overview",
+                    link: "/overview",
+                },
+                {
+                    label: "User",
+                    autogenerate: { directory: "user" },
+                },
+                {
+                    label: "Admin",
+                    autogenerate: { directory: "admin" },
+                },
+                {
+                    label: "Developper",
+                    autogenerate: { directory: "developper" },
+                },
+            ],
+        }),
+    ],
+    redirects: {
+        "/": "/en",
+    },
 });
